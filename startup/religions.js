721f848cc91748e4bc9349cae5dc4e59bde80d27
mdf53 <mdfeist@gmail.com>
2023-10-28 12:55:03 -0600

    Added locally storing the selected religions

---
+++ startup/religions.js
@@ -0,0 +1,31 @@
+//Add the ability to save Which religions you want to see
+// Function to save the checkbox state in local storage
+function saveCheckboxState(checkboxId) {
+    const checkbox = document.getElementById(checkboxId);
+    localStorage.setItem(checkboxId, checkbox.checked);
+}
+
+// Function to load and set the checkbox state from local storage
+function loadCheckboxState(checkboxId) {
+    const checkbox = document.getElementById(checkboxId);
+    const savedState = localStorage.getItem(checkboxId);
+    
+    if (savedState === "true") {
+        checkbox.checked = true;
+    } else {
+        checkbox.checked = false;
+    }
+}
+
+// Attach event listeners to the checkboxes
+const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+checkboxes.forEach((checkbox) => {
+    checkbox.addEventListener('change', function() {
+        saveCheckboxState(checkbox.id);
+    });
+});
+
+// Load the initial state of checkboxes
+checkboxes.forEach((checkbox) => {
+    loadCheckboxState(checkbox.id);
+});
