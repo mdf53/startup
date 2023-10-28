19a65261569bceed9654f3a39a898e8c22ad264a
mdf53 <mdfeist@gmail.com>
2023-10-28 12:18:18 -0600

    Added images for LDS and Islam. Added Islam quote

--- startup/devo.js
+++ startup/devo.js
@@ -2,16 +2,34 @@
 const dailyContent = [
     {
         quote: "And it came to pass that I, Nephi, said unto my father: I will go and do the things which the Lord hath commanded, for I know that the Lord giveth no commandments unto the children of men, save he shall prepare a way for them that they may accomplish the thing which he commandeth them.",
-        citation: "1 Nephi 3:7"
+        citation: "The Book of Mormon: 1 Nephi 3:7",
+        religion: "Christianity (Church of Jesus Christ of Latter-Day Saints",
+        image: "LDS.png"
+
     },
     {
-      quote: "Let all your things be done with charity",
-      citation: "1 Corinthians 16:14"
+        
+      quote: "No one will reap except what they sow.",
+      citation: "Quran 6:164",
+      religion: "Islam",
+      image: "islam.png"
+
     },
+    {
+        
+        quote: "Let all your things be done with charity",
+        citation: "The Bible: 1 Corinthians 16:14",
+        religion: "Christianity",
+        image: "christianity.png"
+  
+      },
     
     {
         quote: "But seek ye first the kingdom of God, and his righteousness; and all these things shall be added unto you.",
-        citation: "Matthew 6:33"
+        citation: "The Bible: Matthew 6:33",
+        religion: "Christianity",
+        image: "christianity.png"
+
     }
     // Add objects for subsequent days here
   ];
@@ -30,15 +48,14 @@ const dailyContent = [
     const dayIndex = dayOfYear % dailyContent.length; 
     const todayQuote = dailyContent[dayIndex].quote;
     const todayCitation = dailyContent[dayIndex].citation;
+    const todayReligion = dailyContent[dayIndex].religion;
+    const todayImage = dailyContent[dayIndex].image;
 
-    // Determine the index of the daily text based on the day of the year
-    const textIndex = dayOfYear % dailyContent.length;
-
-    // Get today's text
-    const todayText = dailyContent[textIndex];
 
     document.getElementById("quote").textContent = todayQuote;
     document.getElementById("citation").textContent = `- ${todayCitation}`;
+    document.getElementById("religion").textContent = todayReligion;
+    document.getElementById("dailyImage").src = todayImage;
     
   }
   
