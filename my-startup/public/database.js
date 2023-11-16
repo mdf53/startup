cefaadf446ac7f7e29e95db0882537cfc43a9e31
mdf53 <mdfeist@gmail.com>
2023-11-16 15:24:39 -0700

    Changed file structure and added simon-db

--- my-startup/public/database.js
+++
@@ -1,3 +0,0 @@
-const config = require('./dbConfig.json');
-const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
-
--- my-startup/public/mongoTest.js
+++ my-startup/public/mongoTest.js
@@ -1,51 +1,41 @@
-const {MongoClient} = require('mongodb');
-const data = require('./listingData');
-
-const dbName = 'bnb';
-const colName = 'listings';
-
-const url = "mongodb+srv://CS260:CS260Password@cluster0.r0w45tj.mongodb.net";
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const { MongoClient } = require('mongodb');
+const config = require('./dbConfig.json');
 
 async function main() {
-    const client = new MongoClient(url);
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
+  // Connect to the database cluster
+  const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+  const client = new MongoClient(url);
+  const db = client.db('rental');
+  const collection = db.collection('house');
+
+  // Test that you can connect to the database
+  (async function testConnection() {
     await client.connect();
-    // Send a ping to confirm a successful connection
-    const result = await client.db(dbName).collection(colName).insertMany(data);
+    await db.command({ ping: 1 });
+  })().catch((ex) => {
+    console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+    process.exit(1);
+  });
 
+  // Insert a document
+  const house = {
+    name: 'Beachfront views',
+    summary: 'From your bedroom to the beach, no shoes required',
+    property_type: 'Condo',
+    beds: 1,
+  };
+  await collection.insertOne(house);
 
-    console.log('Inserted ${result.insertedCount} docs');
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+  // Query the documents
+  const query = { property_type: 'Condo', beds: { $lt: 2 } };
+  const options = {
+    sort: { score: -1 },
+    limit: 10,
+  };
+
+  const cursor = collection.find(query, options);
+  const rentals = await cursor.toArray();
+  rentals.forEach((i) => console.log(i));
 }
+
 main().catch(console.error);
-
-// const { MongoClient, ServerApiVersion } = require('mongodb');
-// const uri = "mongodb+srv://CS260:CS260Password@cluster0.r0w45tj.mongodb.net/?retryWrites=true&w=majority";
-
-// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   }
-// });
-
-// async function run() {
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
-//     await client.connect();
-//     // Send a ping to confirm a successful connection
-//     await client.db("admin").command({ ping: 1 });
-//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// }
-// run().catch(console.dir);
