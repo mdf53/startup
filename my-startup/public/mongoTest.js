747dab944f7651063e338ba9e4386cbb12ffcd66
mdf53 <mdfeist@gmail.com>
2023-11-16 14:28:08 -0700

    Testing mongo

--- my-startup/public/mongoTest.js
+++ my-startup/public/mongoTest.js
@@ -1,23 +1,25 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://CS260:<CS260Password>@cluster0.r0w45tj.mongodb.net/?retryWrites=true&w=majority";
+const MongoClient = require('mongodb');
+const data = require('./listingData');
+
+const dbName = 'bnb';
+const colName = 'listings';
+
+const url = "mongodb+srv://CS260:<CS260Password>@cluster0.r0w45tj.mongodb.net/?retryWrites=true&w=majority";
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-async function run() {
+
+async function main() {
+    const client = new MongoClient(url);
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    const result = await client.db(dbName).collection(colName).insertMany(data);
+
+
+    console.log('Inserted ${result.insertedCount} docs');
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
   }
 }
-run().catch(console.dir);
+main().catch(console.error);
